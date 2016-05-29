9f1825d48c6d1e632ae01176e36d3012bd02dec9
Rickard Liljeberg <rickard@liljeberg.se>
2016-05-29 12:44:25 +0200

    abstract method was missing

--- SortableList.cs
+++ SortableList.cs
@@ -140,7 +140,36 @@ namespace SortableList.Models
 	}
 
 
-	
+	public abstract class SortableListColumnData
+    {
+        protected SortableListColumnData(string type)
+        {
+            Children = new List<SortableListColumnChildren>();
+            Type = type;
+        }
+
+
+        public string Type { get; }
+
+        /// <summary>
+        /// This is the id sent out with an event for the clicked item.
+        /// This is not needed if you have an Url set as no event will be fired in that case.
+        /// </summary>
+        public string Id { get; set; }
+
+
+        /// <summary>
+        ///	If you want any special css classes on this cell you can add them here as a space separated string
+        /// </summary>
+        public string CssClasses { get; set; }
+
+        /// <summary>
+        ///A Column can contain a list of all the children. Theoretically it could be a list of anything, but children is most logical
+        ///If this is set the Label is ignored
+        /// </summary>
+        public IList<SortableListColumnChildren> Children { get; set; }
+
+    }
 	
     public class SortableListLabelColumnData : SortableListColumnData
     {
