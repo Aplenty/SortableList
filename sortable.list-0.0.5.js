002710f14482e6916a8844801d10a5a9d007a12b
Rickard Liljeberg <rickard@liljeberg.se>
2016-05-29 14:07:20 +0200

    split out templates to own file to make it easier for different projects to use different templates

--- sortable.list-0.0.5.js
+++ sortable.list-0.0.5.js
@@ -150,199 +150,24 @@ function SortableList() {
 
         var html = "";
 
+		var addButtonLabel = sortableListSelf.t("[[[Add new]]]");
+        
+		if (Settings != null && Settings.hasOwnProperty("add-label"))
+		{
+			addButtonLabel = Settings["add-label"];
+		}
 
-        var defaultPager = "<div class='pagination-centered'>" +
-            "<div data-bind='text: \" "+sortableListSelf.t("[[[Displaying]]]")+" \" + ($root.TotalItemCount() == 0 ? 0 : ($root.CurrentPage()*$root.ItemsPerPage()-$root.ItemsPerPage()+1)) + \" - \" + ($root.CurrentPage()*$root.ItemsPerPage() > $root.TotalItemCount() ? $root.TotalItemCount() : $root.CurrentPage()*$root.ItemsPerPage()) + \" "+sortableListSelf.t("[[[of///displaying 10-20 OF 40 results]]]")+" \" + $root.TotalItemCount() + \" "+sortableListSelf.t("[[[total results.]]]")+"\"'>" +
-                "</div><ul class='pagination'>" +
-                "<li class='arrow reload-on-dom-insert' data-bind='click: $root.prevPage, css: { unavailable: $root.CurrentPage() <= 1 }'><a href=''>&laquo; "+sortableListSelf.t("[[[Prev]]]")+"</a></li>" +
-                "<!-- ko foreach: pages -->" +
-                "<!-- ko if: (pageNr == 1 && $root.CurrentPage() >= 4 && Math.ceil($root.TotalItemCount()/$root.ItemsPerPage()) > 5)  -->" +
-                "<li class='pagenum' data-bind='click: $root.changePage'><a href='' data-bind='text: (pageNr+\"...\")'></a></li>" +
-                "<!-- /ko -->" +
-                "<!-- ko if: (pageNr > $root.CurrentPage() - 3 && pageNr < $root.CurrentPage() + 3) || (pageNr <= 5 && $root.CurrentPage() <= 3) || (pageNr >= Math.ceil($root.TotalItemCount()/$root.ItemsPerPage())-3 && $root.CurrentPage() >= Math.ceil($root.TotalItemCount()/$root.ItemsPerPage())-2)  -->" +
-                "<li class='pagenum' data-bind='click: $root.changePage, css: { current: $root.CurrentPage() == pageNr }'><a href='#' data-bind='text: pageNr'></a></li>" +
-                "<!-- /ko -->" +
-                "<!-- ko if: (pageNr == Math.ceil($root.TotalItemCount()/$root.ItemsPerPage()) && $root.CurrentPage() <= Math.ceil($root.TotalItemCount()/$root.ItemsPerPage())-3) && Math.ceil($root.TotalItemCount()/$root.ItemsPerPage()) > 5  -->" +
-                "<li class='pagenum' data-bind='click: $root.changePage'><a href='' data-bind='text: (\"...\"+pageNr)'></a></li>" +
-                "<!-- /ko -->" +
-                "<!-- /ko -->" +
-                //"<li class='arrow reload-on-dom-insert' data-bind='click: $root.nextPage, css: { inactive: $root.CurrentPage() >= Math.ceil($root.TotalItemCount()/$root.ItemsPerPage()) }'><a href=''>"+sortableListSelf.t("[[[Next]]]")+" &raquo;</a></li>" +
-                "<li class='arrow reload-on-dom-insert' data-bind='click: $root.nextPage, css: { unavailable: $root.CurrentPage() >= Math.ceil($root.TotalItemCount()/$root.ItemsPerPage()) }'><a href=''>"+sortableListSelf.t("[[[Next]]]")+" &raquo;</a></li>" +
-                "</ul></div>";
 
 
         //default template
         if (typeof template == 'undefined' || template == null || template.length == 0) {
-            var addButtonLabel = sortableListSelf.t("[[[Add new]]]");
-            if (Settings != null && Settings.hasOwnProperty("add-label"))
-                addButtonLabel = Settings["add-label"];
-
-            html +=
-                //"<div class='filter'>" +
-                //    "<input type='text' data-bind='value: searchText, valueUpdate: \"input\", event: {keyup: $root.search}' placeholder='"+sortableListSelf.t("[[[filter]]]")+"' />" +
-                //"</div>" +
-                "<div>" +
-                    "<div class='header'>" +
-                        "<h2 data-bind='text: Title' class='small-11 medium-10 large-12 columns'>" +
-                        "</h2>" +
-                        "<div class='helper'>" +
-                            "<div class='helperActions small-12 medium-2 large-3 large-push-9 columns'>" +
-                                "<!-- ko if:HelpText().length > 0 -->" +
-                                    "<div class='list-tooltip reload-on-dom-insert' data-bind='tooltip: { content: HelpText() }'>" +
-                                        "<img data-bind='attr { src: ($root.GetContentPath() + \"Info2."+iconIconFileEnding+"\" ) }' />" +
-                                    "</div>" +
-                                "<!-- /ko -->" +
-                                "<!-- ko if:AddAllowed() -->" +
-                                        "<button class='button reload-on-dom-insert' data-bind='visible:AddAllowed(), attr: { href: AddUrl() != null && AddUrl().length > 0 ? AddUrl() : \"#\" }, click: function(data, event) { return $root.addClick(data, event, $parents) }'>" +
-                                            addButtonLabel +
-                                        "</button>" +
-                                "<!-- /ko -->" +
-                             "</div>" +
-                             "<div class='filter small-12 large-9 large-pull-3 columns'>" +
-                                // TODO: valueUpdate: "afterkeydown" works in IE9-, but also triggers on shift, alt and so on. Once IE9 support is dropped, change to "input" instead, that only responds to actual changes
-                                "<input class='reload-on-dom-insert' type='text' data-bind='value: searchText, valueUpdate: \"afterkeydown\", event: {keyup: $root.search}' placeholder='"+sortableListSelf.t("[[[filter]]]")+"' />" +
-                            "</div>" +
-                        "</div>"+
-                    "</div>" +
-                    
-                    //"<!-- ko if:HelpText().length > 0 -->" +
-                    //    "<div class='list-tooltip' data-bind='tooltip: { content: HelpText() }'>" +
-                    //        "<img src='/Content/Images/icons/Info.png'>" + 
-                    //    "</div>" +
-                    //"<!-- /ko -->" +
-                    "<table>" +
-                        "<thead>" +
-                            "<!-- ko if: Columns().length > 0 -->" +
-                                "<tr data-bind='foreach: Columns'>" +
-                                    "<td data-bind='css: { ActiveSorting: $root.SortedBy() == SortName(), Desc: $root.SortedDescending(), FoldChildren: ($context.$index() > 0)}'>" +
-                                        "<!-- ko if: Sortable -->" +
-                                            "<a data-bind='click: $root.resort, clickBubble: false'>" +
-                                                "<span data-bind='text: Label'>" +
-                                                "</span>" +
-                                            "</a>" +
-                                        "<!-- /ko -->" +
-                                        "<!-- ko ifnot: Sortable -->" +
-                                            "<span data-bind='text: Label'>" +
-                                            "</span>" +
-                                        "<!-- /ko -->" +
-                                    "</td>" +
-
-
-                                    "<!-- ko if: $root.HideActionsColumn() == false && $parent.Columns().length == ($index()+1) -->" +
-                                            "<td class='listActions' data-bind='css: { \"listFold\": ($root.isFullSize() == false) }'>" +
-                                                "<span data-bind='html: $root.actionText'></span>" +
-                                            "</td>" +
-                                        "<!-- /ko -->" +
-                                    "<!-- /ko -->" +
-                                "</tr>" +
-                            "<!-- /ko -->" +
-                        "</thead>" +
-                        "<tbody data-bind='foreach: Rows'>" +
-							"<!-- ko ifnot:Hidden() -->" +
-								"<tr class='FoldBase' data-bind='foreach: Cells, css: { \"even\": ($index() % 2 == 0) }, attr: { \"data-id\":Id }, click: function(data, event) { return $root.itemClick(data, event, $parents, true) }'>" +
-
-                                    "<!-- ko if:Type() == 'Checkbox' -->" +
-                                    "<td data-bind='css: { \"FoldChildren\": ($context.$index() > 0) }, attr: {\"data-heading\": $root.Columns()[$index()].Label(), \"class\": CssClasses() }'>" +
-                                        "<input type='checkbox' data-bind='attr: { \"data-id\": Id, \"data-actionName\": ActionName}, checked: Checked(), click: function(data, event) { return $root.checkboxClick(data, event, $parents) }, clickBubble: false'></input>" +
-                                    "</td>" +
-                                    "<!-- /ko -->" +
-
-                                    "<!-- ko if:Type() == 'Html' -->" +
-                                    
-                                    "<!-- /ko -->" +									
-									
-                                    "<!-- ko if:Type() == 'Label' -->" +
-									    "<!-- ko if:Children().length > 0 -->" +
-										    "<td data-bind='foreach:Children, css: { \"FoldChildren\": ($context.$index() > 0) }, attr: {\"data-heading\": $root.Columns()[$index()].Label(), \"class\": CssClasses() }'>" +
-											    "<!-- ko if:Interactive -->" +
-												    "<a data-bind='text:Label, attr: { href: Url() != null && Url().length > 0 ? Url() : \"#\" }, click: function(data, event) { return $root.itemClick(data, event, $parents, false) }'>" +
-												    "</a>" +
-											    "<!-- /ko -->" +
-											    "<!-- ko ifnot:Interactive -->" +
-												    "<span data-bind='text:Label'>" +
-												    "</span>" +
-											    "<!-- /ko -->" +
-										    "</td>" +
-									    "<!-- /ko -->" +
-									    "<!-- ko if:Children().length == 0 -->" +
-										    "<!-- ko ifnot:Interactive() -->" +
-											    "<td data-bind='text:Label, css: { \"FoldChildren\": ($context.$index() > 0) }, attr: {\"data-heading\": $root.Columns()[$index()].Label(), \"class\": CssClasses() }'>" +
-											    "</td>" +
-										    "<!-- /ko -->" +
-										    "<!-- ko if:Interactive() -->" +
-											    "<td data-bind='css: { \"FoldChildren\": ($context.$index() > 0) }, attr: {\"data-heading\": $root.Columns()[$index()].Label(), \"class\": CssClasses() }'>" +
-												    "<a data-bind='text:Label, attr: { href: Url() != null && Url().length > 0 ? Url() : \"#\", target: OpenUrlNewWindow() == true ? \"_blank\" : \"_self\"  }, click: function(data, event) { return $root.itemClick(data, event, $parents, false) }, clickBubble: false'>" +
-												    "</a>" +
-											    "</td>" +
-										    "<!-- /ko -->" +
-									    "<!-- /ko -->" +
-                                    "<!-- /ko -->" +
-									"<!-- ko if: $parent.Cells().length == ($index()+1) -->" +
-
-										"<!-- ko if: $root.HideActionsColumn() == false -->" +
-											"<td class='listActions FoldChildren' data-bind='attr: {\"data-heading\": \""+sortableListSelf.t("[[[Actions]]]")+"\"}'>" +
-												"<!-- ko foreach: $parent.ActionGroups -->" +
-													"<span class='action-group'>" +
-														"<!-- ko foreach: Actions() -->" +
-															"<a class='action' data-bind='attr: { href: Url() != null && Url().length > 0 ? Url() : \"#\", title: (Toggled() && ToggledDescription() != \"\" ? ToggledDescription : Description) }, click: function(data, event) { return $root.actionClick(data, event, $parents) }, clickBubble: false'>" +
-																"<!-- ko if: IsTextLabel() == false -->" +
-																	"<img data-bind='attr { src: ($root.GetContentPath() + Type() + (Toggled() ? \"-toggled\" : \"\") + (Disabled() ? \"-disabled\" : \"\") + \"."+iconIconFileEnding+"\"), class: (Disabled() ? Type() +  \" disabled\" : Type()), alt: (Toggled() && ToggledDescription() != \"\" ? ToggledDescription : Description)  }' />" +
-																"<!-- /ko -->" +
-																"<!-- ko if: IsTextLabel() -->" +
-																	"<span data-bind='text: Type()'>" +
-																	"</span>" +
-																"<!-- /ko -->" +
-															"</a>" +
-														"<!-- /ko -->" +
-													"</span>" +
-												"<!-- /ko -->" +
-
-												"<!-- ko foreach: $parent.Actions -->" +
-													"<a class='action' data-bind='attr: { href: Url() != null && Url().length > 0 ? Url() : \"#\", title: (Toggled() && ToggledDescription() != \"\" ? ToggledDescription : Description) }, click: function(data, event) { return $root.actionClick(data, event, $parents) }, clickBubble: false'>" +
-														"<!-- ko if: IsTextLabel() == false -->" +
-															"<img data-bind='attr: { src: ($root.GetContentPath() + Type() + (Toggled() ? \"-toggled\" : \"\") + (Disabled() ? \"-disabled\" : \"\") + \"."+iconIconFileEnding+"\"), class: (Disabled() ? Type() +  \" disabled\" : Type()), alt: (Toggled() && ToggledDescription() != \"\" ? ToggledDescription : Description)  }' />" +
-														"<!-- /ko -->" +
-														"<!-- ko if: IsTextLabel() -->" +
-															"<span data-bind='text: Type()'>" +
-															"</span>" +
-														"<!-- /ko -->" +
-													"</a>" +
-												"<!-- /ko -->" +
-											"</td>" +
-										"<!-- /ko -->" +
-
-
-										"<td class='toggleControl' data-bind='attr: {\"data-heading\": \"ToggleControl\"}'>" +
-											"<div class='toggleListFold' data-bind='click: $root.toggleFolding'>" +
-												"<span class='cavet'>" +
-												"</span>" +
-											"</div>" +
-										"</td>" +
-
-									"<!-- /ko -->" +
-								"</tr>" +
-							"<!-- /ko -->" +
-                        "</tbody>" +
-                    "</table>" +
-                "</div>" +
-                defaultPager;
-
+            
+			template = sortableListTemplate;
         }
-        else if (template == "slim") {
-
-            //this is currently only a placeholder to show that you can have different layouts
-            html +=
-                "<div>" +
-                "Kalle" +
-                "</div>";
-        }
-
-
-
         
 
 
-        $(Container).html(html);
+        $(Container).html(template);
 
     };
 
