6656b61e534a97c4f0d4e0bd1b6776c327e35ce6
Simon Bothén <caresilabs@gmail.com>
2016-05-29 20:26:46 +0200

    Added HTML Render support Added on list loaded action

--- SortableList.cs
+++ SortableList.cs
@@ -235,6 +235,23 @@ namespace SortableList.Models
         /// </summary>
         public string Html { get; set; }
 
+        /// <summary>
+        ///If this is false the Url attribute will be ignored.
+        ///If this is true it will either follow Url on click if set, otherwise fire an event
+        ///The url can be null and this true, as there will then be an event fired.
+        /// </summary>
+        public bool Interactive { get; set; }
+
+        /// <summary>
+        ///Url to be called when clicked
+        /// </summary>
+        public String Url { get; set; }
+
+        /// <summary>
+		/// If Url is set you can here decide if the link should be opened in new window or same window
+		/// </summary>
+		public bool OpenUrlNewWindow { get; set; }
+
     }
 	
 	
